dfe7ab9ad90a35758ee284610b9cd4e27c147f7a
Manujdixit <manujdixit04@gmail.com>
2024-09-08 23:48:10 +0530

    router,server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to run sever", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello, world"))
 }
